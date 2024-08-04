b3eb0ad2bac5640584cf8339a904dd3228513977
Maryna-01 <liashchenkomarina@gmail.com>
2024-08-04 15:06:06 +0200

    abdate render

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,14 +2,16 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 export function renderImages(images) {
   const gallery = document.querySelector('.gallery');
-  gallery.innerHTML = images.map(image => `
+  gallery.innerHTML = images
+    .map(
+      image => `
         <a href="${image.largeImageURL}" class="gallery-item">
             <img src="${image.webformatURL}" alt="${image.tags}" />
             <div class="info">
-                <p><strong>Likes:</strong> ${image.likes}</p>
-                <p><strong>Views:</strong> ${image.views}</p>
-                <p><strong>Comments:</strong> ${image.comments}</p>
-                <p><strong>Downloads:</strong> ${image.downloads}</p>
+                <p class="info-item"><strong>Likes:</strong> ${image.likes}</p>
+                <p class="info-item"><strong>Views:</strong> ${image.views}</p>
+                <p class="info-item"><strong>Comments:</strong> ${image.comments}</p>
+                <p class="info-item"><strong>Downloads:</strong> ${image.downloads}</p>
             </div>
         </a>
         `
